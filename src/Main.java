f4a0f182828b7241b4904867fcf0eb55ffaa3f0e
Gabriel Galli <g7.galli96@gmail.com>
2015-03-22 00:40:52 -0300

    Added license header

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of binary_search_tree.
+ *
+ *  binary_search_tree is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  binary_search_tree is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with binary_search_tree. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class Main {
     public static void main(String args[]) {
         Tree t = new Tree(5);
@@ -33,7 +50,7 @@ class Main {
         System.out.println("t.root.predecessor: " + t.root.predecessor().k);
         System.out.println("t.root.successor: " + t.root.successor().k);
 
-        while (true) {
+         while (true) {
             System.out.println();
             t.remove(t.root);
             if (t.root != null) t.inorderWalk();
